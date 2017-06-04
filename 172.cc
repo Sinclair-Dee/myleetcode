5303f7b55af713aecde9383bae164dc4f77f988d
SinclairDee <dixinkai@outlook.com>
2017-06-04 13:42:36 +0000

    Factorial Trailing Zeroes

---
+++ 172.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int sum = 0;
+        while(n){
+           sum += n/5;
+           n /= 5;
+        }
+    return sum;
+    }
+};
+int main(){
+    int sum;
+    Solution solution;
+    sum = solution.trailingZeroes(2);
+    cout<<sum<<endl;
+    return 0;
+}
