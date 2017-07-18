3a11b2dccec3cbdf3dfd2ca7c7701c1a87eb07c5
SinclairDee <dixinkai@outlook.com>
2017-07-18 12:05:31 +0000

    1twosum

--- 1/1twosum.cc
+++ 1/1twosum.cc
@@ -1,3 +1,4 @@
+//下边这种算法是不正确的，因为题目中没有说vector里的数学是正的还是负的。
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -5,16 +6,33 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> num;
-        bool flag = false
-        for(int i=1;i<(target/2)+1;i++)
-            if(i != target-i){
-                sort(nums,num, i,target-i); 
+        for(int i = 0;i<(target/2)+1;i++){
+            if(sort(nums,num, i,target-i))
+                return num;
+        }
+    }
+private:
+    bool sort(vector<int>& nums,vector<int>& num, int idx1,int idx2){
+        int cnt = 0;
+        //if(idx1 == idx2) return false;
+        for(int i =0;i < nums.size();i++){
+            if(idx1 == nums[i]||idx2 == nums[i]){
+                num.push_back(i);
+                cnt ++;
             }
-    return num;
+        if(cnt == 2) return true;
+        }
+        return false;
     }
 };
-bool sort(vector<int>& nums,vector<int>& num, int i,j){
-    for()
-}
 
+int main(){
+    int num[4]={0,3,2,0};
+    vector<int> nums(num,num+4); //给vector赋初值的一种方式
+    vector<int> sum;
+    int target =6;
+    Solution solution;
+    sum = solution.twoSum(nums, target);
+    return 0;
+}
 
