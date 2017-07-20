40cafa93f95eb8a49a5cd322b579c38b7462d7ca
SinclairDee <dixinkai@outlook.com>
2017-07-20 07:55:22 +0000

    1.two sum

--- 1/tsummap.cc
+++ 1/tsummap.cc
@@ -1,32 +1,22 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <vector>
 using namespace std;
-class Solution {  
+class Solution {
 public:  
     vector<int> twoSum(vector<int>& nums, int target) {  
-        vector<int> res;  
-        int len = nums.size();  
-        unordered_map<int,int> m;  
-        for (int i = 0; i < len; i++){  
-            if (m.count(target - nums[i])){  
-                res.push_back(i);  
-                res.push_back(m[target - nums[i]]);  
-                return res;  
-            }  
-            m[nums[i]] = i;  
-        }  
-        return res;  
-    }  
-};  
+};
 
 int main(){
-    int num[4]={0,3,2,0};
+    int num[4]={1,3,2,6};
     vector<int> nums(num,num+4); //给vector赋初值的一种方式
     vector<int> sum;
-    int target =6;
+    int target =7;
     Solution solution;
     sum = solution.twoSum(nums, target);
+    int sum0 = sum[0];
+    int sum1 = sum[1];
+    cout<<"["<<sum0<<","<<sum1<<"]"<<endl;
     return 0;
 }
 
--- 1/twosum.md
+++ 1/twosum.md
@@ -2,3 +2,23 @@
 unordered＿map和map类似，都是存储的key-value的值，可以通过key快速索引到value。
 不同的是unordered＿map不会根据key的大小进行排序。
 
+代码片段：
+```
+        vector<int> res;  
+        int len = nums.size();  
+        unordered_map<int,int> m;  
+        for (int i = 0; i < len; i++){  
+            if (m.count(target - nums[i])){  
+                res.push_back(m[target - nums[i]]);  
+                res.push_back(i);  
+                return res;  
+            }  
+            m[nums[i]] = i;  
+        }  
+        return res;  
+    }  
+```
+-建立unordered_map数据类型，key为map中的元素，value为其对应的下标。
+-遍历输入nums中的元素，判断target-i是不是nums中的元素，如果是的话则说明已经找到了相加为target的两个数。
+-如果没有找到就把该数字i放到map中，for语句继续运行。直到找到两个目标数。
+-返回两个数组成的vector.
