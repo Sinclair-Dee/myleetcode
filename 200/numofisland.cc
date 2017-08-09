1bee5eb949fa31e86246ac14e101385b6927f67b
SinclairDee <dixinkai@outlook.com>
2017-08-09 15:56:00 +0000

    200.num of islands

--- 200/numofisland.cc
+++ 200/numofisland.cc
@@ -1,31 +1,34 @@
 //思路：
 //union-find求解。将grid 中所有上下相邻的陆地合并，合并后并查集中陆地类簇的个数即为岛的个数。
+
 #include <iostream>
 #include <vector>
+#include <assert.h>
 using namespace std;
+
 
 class UF{ //并查集数据结构
 public:
 //union-find构造函数.
 //参数:the_len(前导数组的初始长度).
 //思路:初始化每个结点的初始前导结点为其自身，也即每个结点单独为一类。
-void UF(int the_len){
-    pri.resize(the_len);
+UF(int the_len){//构造函数，无返回值。
+    pre.resize(the_len);
     for(int i=0;i<the_len;i++){
-        pri[i] = i;
+        pre[i] = i;
     }
 }
 //找到x所属类簇的根结点root，且实现数据压缩(将x至root路径上的所有结点都直接连到root上)。
 int find(int x){
     //查找根节点
     int root = x;
-    while(pri[root] !=root)
-        root = pri[root];
+    while(pre[root] !=root)
+        root = pre[root];
     //压缩
-    while(pri[x] != root){
-        int pri_temp = pri[x];
-        pri[x] = root;
-        x = pri_temp;
+    while(pre[x] != root){
+        int pre_temp = pre[x];
+        pre[x] = root;
+        x = pre_temp;
     }
     //返回根节点
     return root;
@@ -44,7 +47,7 @@ bool isunion(int x,int y){
 }
 //将x 和y 所属的两个类簇合并，也即将y 所属类簇的根结点ry连接到x所属类簇的根结点rx上。
 void uniontwo(int x,int y){
-    pri(x) = pri(find(y));
+    pre[x] = pre[find(y)];
 }
 protected:
 vector<int> pre; //前导数组，pre[i]表示i 与pre[i]属于同一类，且pre[i]为i的上一级。每一个类簇的根root的pre[root]=root
@@ -54,7 +57,7 @@ class Solution{
 public:
 int numIslands(vector<vector<char>>& grid){
     //如果vector长度为0，则返回0.
-    if(grid.size() = 0)
+    if(grid.size() == 0)
         return 0;
     //建立 m,n。分别为grid 的行数与列数
     int m = grid.size();
@@ -71,17 +74,17 @@ UF unionfind(m*n);
 //返回岛屿数量count。
 for(int i=0;i<m;i++)
     for(int j=0;j<n;j++){
-        if(gird[i][j] != '1')
+        if(grid[i][j] != '1')
             continue;
         count++;
-        
-        if(i>0 && gird[i-1][j] = '1'){
-            unionfind.uniontwo((i-1)*m+j-1,i*m+j-1);
+
+        if(i>0 && grid[i-1][j] == '1'){
+            unionfind.uniontwo((i-1)*m+j-1, i*m+j-1);
             count--;
         }
 
-        if(j>0 && gird[i][j-1] = '1'){
-            unionfind.uniontwo((i*m+(j-1)-1,i*m+j-1);   
+        if(j>0 && grid[i][j-1] == '1'){
+            unionfind.uniontwo(i*m+(j-1)-1, i*m+j-1);   
             count--;
         }
     }
@@ -92,16 +95,28 @@ return count;
 //test
 int main(){
     int m,n;
-    vector<vector<char>> grid;
-    cin>>m>>n;
-    for(int i=0;i<m;i++)
-        for(int j=0;j<n;j++){
+	cout<<"请输入行数m：";
+    cin>>m;
+	cout<<endl;
+	cout<<"请输入列数n：";
+	cin>>n;
+	cout<<endl;
+
+	vector<vector<char>> grid(m,vector<char>(n));//必须要指定其大小，不然后续会造成越界。
+	for(int i=0;i<m;i++){
+		cout<<"第"<<i+1<<"行："<<endl;
+		for(int j=0;j<n;j++){
             int in;
-            cout>>in;
-            grid[i][j] = in;
+            cin>>in;
+			assert(in == 0 || in == 1);
+			if(in == 1) grid[i][j] = '1';
+			if(in == 0) grid[i][j] = '0';
+			//if(in == 1) grid[i].push_back('1');
+			//if(in == 0) grid[i].push_back('0');
         }
+	}
     Solution solution;
     int count = solution.numIslands(grid);
-    cout<<count<<endl;
+    cout<<"共有"<<count<<"个岛屿"<<endl;
     return 0;
 }
